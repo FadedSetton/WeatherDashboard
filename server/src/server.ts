a0330283b85daf989e5d4b3d3ea4047fb651a1f4
Christopher Munoz <chrismunozpro@gmail.com>
2025-04-08 15:46:36 -0700

    set up weather routes and server backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
+
 
 // Import the routes
 import routes from './routes/index.js';
@@ -10,11 +14,20 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use('/api', routes);
+
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+})
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
